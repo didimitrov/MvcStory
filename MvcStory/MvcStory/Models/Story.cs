d8179cc73acf699cc6df63c6e9ae53318d396e72
didimitrov <di_dimitrov_@abv.bg>
2015-01-21 09:40:41 +0200

    Instal EF

--- MvcStory/MvcStory/Models/Story.cs
+++ MvcStory/MvcStory/Models/Story.cs
@@ -1,11 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Data.Entity;
 
 namespace MvcStory.Models
 {
     public class Story
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Text  { get; set; }
+        public String Image     
+        {
+            get
+            {
+                return "~/Content/Images/hire_asp_net_developer.png";
+            }
+
+        }       
+    }
+
+    public class StoryDbContext : DbContext
+    {
+        public DbSet<Story> Stories { get; set; }
+
     }
 }
